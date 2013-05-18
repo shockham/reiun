888f9e8b9b08a0f919808cfaf06b53d71687413e
Samuel Hockham <sam@cube.tv>
2013-05-18 11:39:29 +0100

    added more staccato notes

--- js/base.js
+++ js/base.js
@@ -8,8 +8,8 @@
         output.gain.value = 1;
 
         create_delay(input).connect(output);
-        create_synth(500).connect(output);
-        create_synth(2000).connect(output);
+        create_synth(500, true).connect(output);
+        create_synth(2000, false).connect(output);
 
         output.connect(ctx.destination);
 
@@ -35,7 +35,7 @@
             return Math.pow(2, ((key_no-49)/12)) * 440;
         }
 
-        function create_synth(speed){
+        function create_synth(speed, staccato){
             // sin
             var osc = ctx.createOscillator();
             osc.type = osc.SINE;
@@ -49,6 +49,13 @@
 
             var osc_gain = ctx.createGainNode();
             osc_gain.gain.value = 0.35;
+
+            if(staccato){
+                setInterval(function(){
+                    if(osc_gain.gain.value === 0) osc_gain.gain.value = 0.35;
+                    else osc_gain.gain.value = 0;
+                }, speed/2);
+            }
 
             osc.connect(osc_gain);
 
